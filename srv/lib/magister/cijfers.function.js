bd38b9802e520441d5b95be39434dc8d5720c720
Sj3rd <sjoerdabolten@gmail.com>
2019-03-23 20:29:27 +0100

    Update cijfers.function.js

--- srv/lib/magister/cijfers.function.js
+++ srv/lib/magister/cijfers.function.js
@@ -16,7 +16,7 @@ module.exports = async function(params, res) {
         m.courses()
         .then(courses => courses.find(c => c.current).grades())
         .then(grades => {
-            response[grades] = grades
+            response["grades"] = grades
             res.writeHead(200)
             res.end(JSON.stringify(response))
         }).catch((err) => { // something went wrong
