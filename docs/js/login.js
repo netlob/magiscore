91d710534aa786405368fde69187b1fabdfea488
minestarnl <bramkoene@gmail.com>
2019-03-23 20:23:49 +0100

    Update login.js

--- docs/js/login.js
+++ docs/js/login.js
@@ -896,7 +896,7 @@ function login(creds) {
         var settings = {
             "async": true,
             "crossDomain": true,
-            "url": "/api/cijfers",
+            "url": "https://magistat.bramkoene.nl/api/cijfers",
             "method": "POST",
             "headers": {
                 "username": creds.username,
