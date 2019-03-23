2e45ea4bf2258063fee0994a68e374d3229bffb6
minestarnl <bramkoene@gmail.com>
2019-03-23 18:38:54 +0100

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -3825,7 +3825,6 @@ function generateHTML(vakName){
     return `<!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
       <h1 class="h3 mb-0 text-gray-800">${vakName.capitalize()}</h1>
-      <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
     </div>
     <!-- Content Row -->
     <div class="row">
