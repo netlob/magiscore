cfb86303c051c85f5f2e81fd5022c450eb819b00
minestarnl <bramkoene@gmail.com>
2019-03-23 22:02:20 +0100

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -85,6 +85,17 @@ function generateHTML(vakName){
       </div>
     </div>
 
+    <div class="row">
+        <div class="col-lg-6 mb-4">
+            <div class="card bg-primary text-white shadow">
+            <div class="card-body">
+                Primary
+                <div class="text-white-50 small">#4e73df</div>
+            </div>
+            </div>
+        </div>
+    </div>
+
     <!-- Content Row -->
 
     <div class="row">
