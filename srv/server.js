0a2abb25448b59eb8c7a994445f7563c4c5a2418
Sj3rd <sjoerdabolten@gmail.com>
2019-03-24 12:17:13 +0100

    .

--- srv/server.js
+++ srv/server.js
@@ -27,8 +27,14 @@ http.createServer((req, res) => {
         .then(mAuth => {
             req.headers.code = mAuth
             cijfers(req.headers, res)
-            .catch(err => { res.writeHead(500), res.end('error: ' + err.toString()); });
-        }).catch(err => { res.writeHead(500); res.end('error: ' + err.toString()); });
+            .catch(err => {
+                res.writeHead(500);
+                res.end('error: ' + err.toString());
+            });
+        }).catch(err => {
+            res.writeHead(500);
+            res.end('error: ' + err.toString());
+        });
     } else {
         res.end('MAGBOT STAT API');
     }
