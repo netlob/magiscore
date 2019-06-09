a5b3f42039dae9410019914b5c14783ca5d60882
Sj3rd <sjoerdabolten@gmail.com>
2019-06-09 15:08:02 +0200

    Update sw.js

--- docs/js/sw.js
+++ docs/js/sw.js
@@ -1,24 +1,24 @@
 var cacheName = 'base'; 
 
 var cacheFiles = [
-	'./',
-    './index.html',
-    './vendor/fontawesome-free/css/all.min.css',
+	'../',
+    '../index.html',
+    '../vendor/fontawesome-free/css/all.min.css',
     'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i',
-    './css/sb-admin-2.css',
-    './css/main.css',
-    './vendor/jquery/jquery.min.js',
-    './vendor/bootstrap/js/bootstrap.bundle.min.js',
-    './vendor/jquery-easing/jquery.easing.min.js',
-    './js/sb-admin-2.js',
-    './vendor/chart.js/Chart.min.js',
-    './js/algebra.js',
-    './js/imagecache.js',
-    './js/chartannotation.js',
-    './js/jspdf.min.js',
-    './js/main.js',
-    './img/icons/icon-128x128.png',
-    './manifest.webmanifest'
+    '../css/sb-admin-2.css',
+    '../css/main.css',
+    '../vendor/jquery/jquery.min.js',
+    '../vendor/bootstrap/js/bootstrap.bundle.min.js',
+    '../vendor/jquery-easing/jquery.easing.min.js',
+    '../js/sb-admin-2.js',
+    '../vendor/chart.js/Chart.min.js',
+    '../js/algebra.js',
+    '../js/imagecache.js',
+    '../js/chartannotation.js',
+    '../js/jspdf.min.js',
+    '../js/main.js',
+    '../img/icons/icon-128x128.png',
+    '../manifest.webmanifest'
 ]
  
 self.addEventListener('install', function(e) {
