d979653bba015f0aa078985c14d808e6a894cb17
KassaDiMama <43823950+KassaDiMama@users.noreply.github.com>
2019-09-01 12:24:12 +0200

    hell yeah

--- app/Magiscore/platforms/android/app/src/main/assets/www/js/refresh.js
+++ app/Magiscore/platforms/android/app/src/main/assets/www/js/refresh.js
@@ -1,28 +1,32 @@
 function refreshToken() {
-    var tokens = JSON.parse(localStorage.getItem("tokens"))
-    var refresh_token = tokens.refresh_token;
+    return new Promise((resolve, reject) => {
+        var tokens = JSON.parse(localStorage.getItem("tokens"))
+        var refresh_token = tokens.refresh_token;
 
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://accounts.magister.net/connect/token",
-        "method": "POST",
-        "headers": {
-            "cache-control": "no-cache"
-        },
-        "data": {
-            "refresh_token": refresh_token,
-            "client_id": "M6LOAPP",
-            "grant_type": "refresh_token"
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://accounts.magister.net/connect/token",
+            "method": "POST",
+            "headers": {
+                "cache-control": "no-cache"
+            },
+            "data": {
+                "refresh_token": refresh_token,
+                "client_id": "M6LOAPP",
+                "grant_type": "refresh_token"
+            }
         }
-    }
 
-    $.ajax(settings).done(function (response) {
-        var tokens = {
-            access_token: response.access_token,
-            refresh_token: response.refresh_token,
-            id_token: response.id_token
-        }
-        localStorage.setItem("tokens", JSON.stringify(tokens))
-    });
+        $.ajax(settings).done(function (response) {
+            var tokens = {
+                access_token: response.access_token,
+                refresh_token: response.refresh_token,
+                id_token: response.id_token
+            }
+            localStorage.setItem("tokens", JSON.stringify(tokens))
+            resolve()
+        });
+    })
+
 }
